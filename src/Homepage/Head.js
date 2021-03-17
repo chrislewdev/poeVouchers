78b33430ef1120d635243f67c50a4484a420f3fe
ChrisLew <chris.lew.dev@gmail.com>
2021-03-17 18:32:54 +0800

    Updated buttons with actual functionality

--- src/Homepage/Head.js
+++ src/Homepage/Head.js
@@ -1,15 +1,42 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { auth } from "../Firebase";
 import HeaderButton from "../Global/HeaderButton";
 import HeadTitle from "../Global/HeadTitle";
 import "./Head.css";
 
 function Head(props) {
+  const history = useHistory();
+
+  const isLoggedIn = props.currentUser;
+
+  const signOutFunction = () => {
+    auth
+      .signOut()
+      .then(() => alert("Logout successful"))
+      .then(() => history.push("/"));
+  };
+
   return (
-    <div className={"head-container"}>
+    <div className="head-container">
       <HeadTitle headTitle="poeVouchers" />
-      <HeaderButton buttonName="Create" />
-      <HeaderButton buttonName="Sign Up" />
-      <HeaderButton buttonName="Sign In" />
+      <div className="head-buttons-container">
+        {isLoggedIn ? <HeaderButton buttonName="Create" /> : <></>}
+        <HeaderButton
+          buttonName="User"
+          handleClick={() => history.push("/user")}
+        />
+        <HeaderButton
+          buttonName="Register"
+          handleClick={() => history.push("/signup")}
+        />
+        <HeaderButton
+          buttonName={isLoggedIn ? "Logout" : "Login"}
+          handleClick={
+            isLoggedIn ? signOutFunction : () => history.push("/signin")
+          }
+        />
+      </div>
     </div>
   );
 }
