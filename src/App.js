c82440f5d91c7a56fb5ae23aa35d4ac6a0377003
ChrisLew <chris.lew.dev@gmail.com>
2021-03-17 18:33:09 +0800

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Homepage from "./Homepage/Homepage";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import Userpage from "./UserPage/Userpage";
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/user" component={Userpage} />
+        <Route path="/" component={Homepage} />
+      </Switch>
+    </Router>
   );
 }
 
