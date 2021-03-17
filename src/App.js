2b4f435d37b42dd1cf881690668ceb69154c9686
ChrisLew <chris.lew.dev@gmail.com>
2021-03-17 15:06:11 +0800

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Homepage from "./Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Homepage />
     </div>
   );
 }
