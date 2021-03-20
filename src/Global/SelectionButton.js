7055e7af79453c02b763790096fdbe592897691e
ChrisLew <chris.lew.dev@gmail.com>
2021-03-20 15:33:06 +0800

    Added active button effect

--- src/Global/SelectionButton.js
+++ src/Global/SelectionButton.js
@@ -3,7 +3,14 @@ import "./SelectionButton.css";
 
 function SelectionButton(props) {
   return (
-    <div className="selection-button" onClick={props.handleClick}>
+    <div
+      className={
+        props.activeButton === props.buttonName
+          ? "selection-button-clicked"
+          : "selection-button"
+      }
+      onClick={props.handleClick}
+    >
       {props.buttonName}
     </div>
   );
