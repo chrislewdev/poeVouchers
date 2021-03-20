8a764a0e5979480d9aeff9899eff648228aadcb7
ChrisLew <chris.lew.dev@gmail.com>
2021-03-20 15:32:05 +0800

    Added file to store arrays

---
+++ src/Global/Arrays.js
@@ -0,0 +1,28 @@
+export const categoriesArray = [
+  "temple",
+  "syndicate",
+  "boss carry",
+  "bench craft",
+  "challenge",
+];
+
+export const templeArray = [
+  "double corruption",
+  "gem corruption",
+  "sacrifice chamber",
+];
+
+export const syndicateArray = [
+  "white sockets",
+  "leo slam",
+  "hillock weapon",
+  "hillock armour",
+  "hillock flask",
+  "hillock map",
+];
+
+export const bossArray = ["all bosses", "sirus", "uber atziri"];
+
+export const benchArray = ["all crafts", "all crafts except"];
+
+export const challengeArray = ["encounter", "end game grind"];
