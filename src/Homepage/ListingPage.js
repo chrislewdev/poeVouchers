44fe87fb585505db9ae438c8b3191e75f0f04aa6
ChrisLew <chris.lew.dev@gmail.com>
2021-04-12 15:44:52 +0800

    Updated with better user experience + displaying more details

--- src/Homepage/ListingPage.js
+++ src/Homepage/ListingPage.js
@@ -15,45 +15,56 @@ function ListingPage({ toggleListingPage, docID, vouches }) {
   const { currentUserData } = useContext(UserContext);
 
   useEffect(() => {
-    if (value != undefined) {
-      const listing = {
-        price: value[0].price,
-        title: value[0].title,
-        detail: value[0].detail,
-        sellerUID: value[0].sellerUID,
-        sellerUsername: value[0].sellerUsername,
-        vouched: value[0].vouched,
-        vouchedByUsername: value[0].vouchedByUsername,
-        vouchedByUID: value[0].vouchedByUID,
-        docID: value[0].docID,
-      };
+    if (value !== undefined) {
+      if (value.length !== 0) {
+        const listing = {
+          price: value[0].price,
+          title: value[0].title,
+          detail: value[0].detail,
+          sellerUID: value[0].sellerUID,
+          sellerUsername: value[0].sellerUsername,
+          vouched: value[0].vouched,
+          vouchedByUsername: value[0].vouchedByUsername,
+          vouchedByUID: value[0].vouchedByUID,
+          docID: value[0].docID,
+          sellerDiscord: value[0].sellerDiscord,
+          sellerPoeProfile: value[0].sellerPoeProfile,
+        };
 
-      setListing(listing);
+        setListing(listing);
+      }
     }
   }, [value]);
 
   const handleVouchClick = () => {
-    if (listing.vouched == false) {
-      if (listing.sellerUsername !== currentUserData[0].username) {
-        db.collection("listings").doc(docID).update({
-          vouched: true,
-          vouchedByUsername: currentUserData[0].username,
-          vouchedByUID: currentUserData[0].userUID,
-        });
+    if (value.length !== 0) {
+      if (listing.vouched == false) {
+        if (listing.sellerUsername !== currentUserData[0].username) {
+          db.collection("listings").doc(docID).update({
+            vouched: true,
+            vouchedByUsername: currentUserData[0].username,
+            vouchedByUID: currentUserData[0].userUID,
+          });
+        }
       }
-    }
-    if (listing.vouched == true) {
-      if (listing.vouchedByUID == currentUserData[0].userUID) {
-        db.collection("listings").doc(docID).update({
-          vouched: false,
-          vouchedByUsername: "",
-          vouchedByUID: "",
-        });
+      if (listing.vouched == true) {
+        if (listing.vouchedByUID == currentUserData[0].userUID) {
+          db.collection("listings").doc(docID).update({
+            vouched: false,
+            vouchedByUsername: "",
+            vouchedByUID: "",
+          });
+        }
       }
+    } else {
+      alert("Listing might be completed or deleted !");
+      toggleListingPage();
     }
   };
 
-  return (
+  return dataLoading ? (
+    <></>
+  ) : value.length !== 0 ? (
     <div className="lp-background">
       <div className="lp-wrapper">
         {/* <div className="lp-title">Listing</div> */}
@@ -64,8 +75,12 @@ function ListingPage({ toggleListingPage, docID, vouches }) {
           <div className="lp-box-one">{listing.title}</div>
           <div className="lp-box-two">{listing.sellerUsername}</div>
           <div className="lp-box-three">{`Vouches: ${vouches}`}</div>
-          <div className="lp-box-four">Discord: aaaaaaaaaaaa </div>
-          <div className="lp-box-five">POE Profile</div>
+          <div className="lp-box-four">{listing.sellerDiscord}</div>
+          <div className="lp-box-five">
+            <a href={listing.sellerPoeProfile} target="_blank">
+              POE Profile
+            </a>
+          </div>
           <div className="lp-box-six">{listing.price}</div>
           <div className="lp-box-seven">{listing.detail}</div>
           <div className="lp-box-eight">
@@ -78,6 +93,15 @@ function ListingPage({ toggleListingPage, docID, vouches }) {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  ) : (
+    <div className="listing-not-found-wrapper">
+      <div className="lp-back-button">
+        <HeaderButton buttonName="<" handleClick={toggleListingPage} />
+      </div>
+      <div className="listing-not-found-title">
+        Listing might have been completed or deleted !
       </div>
     </div>
   );
